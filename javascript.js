5d3808c6efc5cd46bd0a829dc7a64cccc7986ed1
unknown <zivarymostafa78@gmail.com>
2020-02-03 12:29:10 +0330

    first row of first section created

--- javascript.js
+++ javascript.js
@@ -1,61 +1,64 @@
-var scrolled = 0;
+/*var scrolled = 0;
 var row = 0;
 const numberOfRows = 2;
 var loading = false;
-var delayInMilliseconds = 300;
+var delayInMilliseconds = 1000;
 
 window.addEventListener("scroll" , ()=>{
 
-    if (scrolled - window.scrollY < 0){
-        // scroll to bottom
+        if (scrolled - window.scrollY < 0) {
+            // scroll to bottom
 
-        scrolled = window.scrollY;
+            scrolled = window.scrollY;
 
-        if (row < numberOfRows ){
-            row++;
+            if (row < numberOfRows) {
+                row++;
+            }
+
+            var element = "#section" + row;
+            console.log(element);
+            if (!loading) {
+                loading = true;
+                $('html, body').animate({
+                    scrollTop: $(element).offset().top
+                }, delayInMilliseconds);
+
+                setTimeout(function () {
+                    loading = false;
+                }, delayInMilliseconds + 100);
+            }
+
+
+        } else if (scrolled - window.scrollY > 0) {
+            // scroll to top
+            scrolled = window.scrollY;
+
+            if (row > 0) {
+                row--;
+            }
+
+            var element = "#section" + row;
+
+            console.log(element);
+            if (!loading) {
+                loading = true;
+                $('html, body').animate({
+                    scrollTop: $(element).offset().top
+                }, delayInMilliseconds);
+
+
+                setTimeout(function () {
+                    loading = false;
+                }, delayInMilliseconds + 100);
+
+            }
         }
 
-        var element = "#section" + row;
-        console.log(element);
-        if (!loading) {
-            loading = true;
-            $('html, body').animate({
-                scrollTop: $(element).offset().top
-            }, delayInMilliseconds);
-
-            setTimeout(function() {
-                loading = false;
-            }, delayInMilliseconds + 100);
-        }
+});*/
 
 
-    }else if (scrolled - window.scrollY > 0){
-        // scroll to top
-        scrolled = window.scrollY;
-
-        if (row > 0 ){
-            row--;
-        }
-
-        var element = "#section" + row;
-
-        console.log(element);
-        if (!loading) {
-            loading = true;
-            $('html, body').animate({
-                scrollTop: $(element).offset().top
-            }, delayInMilliseconds);
-
-
-            setTimeout(function() {
-                loading = false;
-            }, delayInMilliseconds + 100);
-
-        }
-
-
-
-    }
-
+$('.toggle').click(function(){
+    $('.toggle').toggleClass('active');
+    $('body').toggleClass('night');
 });
 
