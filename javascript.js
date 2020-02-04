8127783ad5a9dcffc013b6be85ba6ad88768f9f8
unknown <zivarymostafa78@gmail.com>
2020-02-04 20:40:00 +0330

    created slide bar for first section and night mode for it

--- javascript.js
+++ javascript.js
@@ -1,64 +1,107 @@
-/*var scrolled = 0;
+var scrolled = 0;
 var row = 0;
 const numberOfRows = 2;
 var loading = false;
 var delayInMilliseconds = 1000;
 
-window.addEventListener("scroll" , ()=>{
+window.addEventListener("scroll", () => {
 
-        if (scrolled - window.scrollY < 0) {
-            // scroll to bottom
-
-            scrolled = window.scrollY;
-
-            if (row < numberOfRows) {
-                row++;
-            }
-
-            var element = "#section" + row;
-            console.log(element);
-            if (!loading) {
-                loading = true;
-                $('html, body').animate({
-                    scrollTop: $(element).offset().top
-                }, delayInMilliseconds);
-
-                setTimeout(function () {
-                    loading = false;
-                }, delayInMilliseconds + 100);
-            }
+    var $el = $('div.menu');
 
 
-        } else if (scrolled - window.scrollY > 0) {
-            // scroll to top
-            scrolled = window.scrollY;
+    if (scrolled - window.scrollY < 0) {
+        // scroll to bottom
 
-            if (row > 0) {
-                row--;
-            }
+        scrolled = window.scrollY;
 
-            var element = "#section" + row;
-
-            console.log(element);
-            if (!loading) {
-                loading = true;
-                $('html, body').animate({
-                    scrollTop: $(element).offset().top
-                }, delayInMilliseconds);
-
-
-                setTimeout(function () {
-                    loading = false;
-                }, delayInMilliseconds + 100);
-
-            }
+        if (row < numberOfRows) {
+            row++;
         }
 
-});*/
+        var element = "#section" + row;
+        console.log(element);
+        if (!loading) {
+            loading = true;
+            $('html, body').animate({
+                scrollTop: $(element).offset().top
+            }, delayInMilliseconds);
+
+            setTimeout(function () {
+                loading = false;
+            }, delayInMilliseconds + 100);
+        }
+
+    } else if (scrolled - window.scrollY > 0) {
+        // scroll to top
+        scrolled = window.scrollY;
+
+        if (row > 0) {
+            row--;
+        }
+
+        var element = "#section" + row;
+
+        console.log(element);
+        if (!loading) {
+            loading = true;
+            $('html, body').animate({
+                scrollTop: $(element).offset().top
+            }, delayInMilliseconds);
 
 
-$('.toggle').click(function(){
+            setTimeout(function () {
+                loading = false;
+            }, delayInMilliseconds + 100);
+
+        }
+
+    }
+
+    if (row == 0){
+        $el.removeClass("sticky");
+    }else{
+        $el.addClass("sticky");
+
+    }
+
+});
+
+$('.toggle').click(function () {
     $('.toggle').toggleClass('active');
     $('body').toggleClass('night');
 });
+
+
+$(".slider").owlCarousel({
+
+    loop: true,
+    margin: 10,
+    rtl: true,
+
+    responsive: {
+        0: {
+            items: 1,
+            autoplay: false
+        },
+        600: {
+            items: 3,
+            autoplay: false
+        },
+        1000: {
+            items: 2
+        }
+    }
+
+});
+
+
+$(window).scroll(function () {
+
+
+});
+
+
+
+
+
 
